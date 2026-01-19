297e49cf65aa5b69ab2ba00a09e059938c9e8d4f
Sensei <sergej.volkov1706@mail.ru>
2026-01-19 17:53:48 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,4 +1,8 @@
 INSERT INTO cars (brand, model, year, color, price) VALUES
-('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
-('BMW', 'X5', 2022, 'Black', 4500000.50),
-('Lada', 'Vesta', 2021, 'White', 800000.00);
+('Ford', 'Focus', 2019, 'Blue', 1200000.00),
+('Mercedes-Benz', 'E-Class', 2023, 'Gray', 5200000.75),
+('Hyundai', 'Solaris', 2022, 'Red', 950000.00),
+('Kia', 'Rio', 2021, 'Brown', 980000.25),
+('Audi', 'Q7', 2020, 'Dark Blue', 3800000.00),
+('Skoda', 'Octavia', 2022, 'Green', 1600000.50),
+('Nissan', 'X-Trail', 2021, 'Orange', 2100000.00);
